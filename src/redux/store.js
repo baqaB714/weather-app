3251508b72649d231d6738d196807cabdb6121dd
baqaB714 <baqa4731@gmail.com>
2024-04-03 23:16:53 +0330

    after install redux-thunk 14030115

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,6 @@
-const store = createStore
+import { applyMiddleware, createStore } from "redux";
+import weatherReducer from "./weather/weatherReducer";
+import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
+import { thunk } from "redux-thunk";
+
+const store = createStore(weatherReducer , composeWithDevTools(applyMiddleware(thunk)))
