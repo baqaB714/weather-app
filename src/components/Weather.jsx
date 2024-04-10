76acd22741500981c1c42bb1f98aa81c94997131
baqaB714 <baqa4731@gmail.com>
2024-04-11 00:18:02 +0330

    befor Solve the store problem

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import getWeatherInfo from '../redux/weather/weatherAction';
 
 const Weather = () => {
-    const{loading , data , error} = useSelector(state=>state);
+    const { loading, data, error } = useSelector(state => state);
     const dispatch = useDispatch()
-    console.log(5555);
+    const [query, setQuery] = useState('')
+
+    const handleGetWeather = (e) => {
+        e.preventDefault()
+        dispatch(getWeatherInfo(query))
+        console.log(data);
+    }
 
 
     return (
@@ -13,25 +20,31 @@ const Weather = () => {
             <div className='flex w-full justify-center'>
                 <div className="flex items-center ps-4 border border-violet-700 rounded w-full md:w-1/2 mx-2 my-2 transition hover:bg-[#ebdff88f] bg-[#b26bfd2f]">
                     <input id="bordered-radio-1" type="radio" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 " defaultChecked />
-                    <label htmlFor ="bordered-radio-1" className="w-full py-4 ms-2 text-sm font-bold text-gray-900 ">درج شهر</label>
+                    <label htmlFor="bordered-radio-1" className="w-full py-4 ms-2 text-sm font-bold text-gray-900 ">درج شهر</label>
                 </div>
                 <div className="flex items-center ps-4 border border-blue-700 rounded w-full md:w-1/2 mx-2 my-2 transition hover:bg-[#ebdff88f] bg-[#799dff65]">
-                    <input  id="bordered-radio-2" type="radio" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 " />
-                    <label htmlFor ="bordered-radio-2" className="w-full py-4 ms-2 text-sm font-bold text-gray-900 ">درج مختصات</label>
+                    <input id="bordered-radio-2" type="radio" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 " />
+                    <label htmlFor="bordered-radio-2" className="w-full py-4 ms-2 text-sm font-bold text-gray-900 ">درج مختصات</label>
                 </div>
             </div>
             <div className='w-full px-2'>
-                <form>
+                <div>
                     <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="search" id="search" className="block w-full p-4 ps-10 text-lg text-gray-900 border rounded-lg " placeholder="نام شهر یا کشور" required />
-                        <button type="submit" className="text-white absolute end-2.5 bottom-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">جست و جو</button>
+                        <input
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                            type="search"
+                            id="search"
+                            className="block w-full p-4 ps-10 text-lg text-gray-900 border rounded-lg "
+                            placeholder="نام شهر یا کشور" required />
+                        <button onClick={handleGetWeather} type="submit" className="text-white absolute end-2.5 bottom-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">جست و جو</button>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     );
--- src/redux/weather/weatherAction.js
+++ src/redux/weather/weatherAction.js
@@ -24,9 +24,10 @@ const getWeatherInfo = (query) => {
     return dispatch => {
         dispatch(sendWeatherRequest());
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=7ebcd0b29f8e07a508956a38313b3ce0
-        `).then(res=>{
+        `).then(res => {
+            console.log(res);
             dispatch(receiveWeatherResponse(res.data))
-        }).catch(error=>{
+        }).catch(error => {
             dispatch(receiveWeatherError(error.message))
         })
     }
